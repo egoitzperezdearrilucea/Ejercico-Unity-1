b52723d3b79be963d7e6e4c1e4559c32139063ab
Egoitz <egoitz.perezdearrilucea@ikasle.egibide.org>
2023-09-17 14:05:14 +0200

    Scriptable object
    Añadido el scriptable object en el que se especifica el nº de objetos

--- Ejercico Unity1/Assets/SriptableObjects/spawner.cs
+++ Ejercico Unity1/Assets/SriptableObjects/spawner.cs
@@ -4,24 +4,20 @@ using UnityEngine;
 
 public class spawner : MonoBehaviour
 {
-    
+    public SpawnerSettings settings;
     public GameObject prefab;
     private int n_circulos;
     
     // Start is called before the first frame update
     void Start()
     {
-        n_circulos = Random.Range(1, 10);
+        //n_circulos = Random.Range(1, 10);
+        n_circulos =settings.numeroObjetos;
+        
         for (int i = 0; i < n_circulos; i++)
         {
-            GameObject circulo_actual = Instantiate(prefab, transform.position = new Vector3(Random.Range(-5,5),Random.Range(-2,2),0), Quaternion.identity);
+            GameObject circulo_actual = Instantiate(prefab, transform.position = new Vector3(Random.Range(-10,10),Random.Range(-4,5),0), Quaternion.identity);
             
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
